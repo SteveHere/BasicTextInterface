af2f732221ee3aaf67b9ea8afa6f9c9ff4d7cfc3
SteveHere <jwkwan97@hotmail.com>
2015-08-28 21:41:01 +0800

    Little mod
    just changed the while loop parameters

--- project/Project.c
+++ project/Project.c
@@ -8,18 +8,16 @@
 
 #include "Environment.c"
 
-//typedef enum {LOGIN, EXIT}
-
 int main(void){
    char login;
-   do{
-   printf("Do you want to login(Y) or exit(N)? ");
+   while(login!='N'){
+      printf("Do you want to login(Y) or exit(N)? ");
       login = Input();
-      if(login=='Y'){ Environment();} //modify "1" parameter once login method has been set up
+      if(login=='Y'){ Environment();} //modify parameter once login method has been set up
       else{
          //puts("Incorrect username or password. Try again."); //only use when login method is complete
          
          }
-   }while(login!='N');
+   }
    return 0;
 }
